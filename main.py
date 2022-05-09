539c3e0a72d0b7e42edf20b8005c0af5e5e2e272
unknown <tannguyen901@gmail.com>
2022-05-09 09:52:31 -0700

    added scoreboard and food

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
+from scoreboard import ScoreBoard
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,16 +12,24 @@ screen.title("Welcome to my Snake Game!")
 screen.tracer(0)
 
 snake = Snake()
-
+food = Food()
+scoreboard = ScoreBoard()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
+
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()
