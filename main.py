6e0975a939ccff87e18c80b0b6ee9074e23e2bfe
unknown <tannguyen901@gmail.com>
2022-05-09 10:23:55 -0700

    added collision and extending snake features:

--- main.py
+++ main.py
@@ -27,9 +27,23 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with food
+    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() < -290 or snake.head.xcor() > 290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
+
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
